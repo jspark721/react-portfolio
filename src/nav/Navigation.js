2b1eb365584bd2e15bafcb6c02dae11adceef503
Julie Park <jspark721@gmail.com>
2018-10-31 13:20:38 -0700

    add navigation

---
+++ src/nav/Navigation.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './navigation.css';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav>
+        <h2 className="logo">JP</h2>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
