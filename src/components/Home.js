36a29f42f9d0e41a4a9422094050517141ebf041
Julie Park <jspark721@gmail.com>
2018-11-07 15:56:23 -0800

    update styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Background from '../img/hero.jpg';
 import '../styles/home.scss';
 
+import About from './About';
+
 class Home extends Component {
   render() {
     return(
@@ -15,13 +17,12 @@ class Home extends Component {
             <p className="button"><a href="/about" className="main-btn">Learn More</a></p>
           </div>
         </header>
+        /*<section className="main">
+          <h2>I'm a software developer, specializing in full-stack web development with a passion for simple, elegant, & functional design.</h2>
+        </section>*/
       </div>
     );
   }
 };
 
 export default Home;
-
-// <section className="main">
-//   <h2>I'm a software developer, specializing in full-stack web development with a passion for simple, elegant, & functional design.</h2>
-// </section>
